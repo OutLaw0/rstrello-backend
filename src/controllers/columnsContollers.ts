9ff49f3711b7873bf7d4604007a7353b548ce051
Valentin Ak <vaakimov@gmail.com>
2022-11-30 01:10:19 +0300

    feat: add 404 columns

--- src/controllers/columnsContollers.ts
+++ src/controllers/columnsContollers.ts
@@ -1,10 +1,9 @@
-import { Response, Request } from 'express';
-import * as columnService from '../services/column.service';
-import { checkBody, createError } from '../services/error.service';
-
+import { Response, Request } from "express";
+import * as columnService from "../services/column.service";
+import { checkBody, createError } from "../services/error.service";
 
 export const getColumns = async (req: Request, res: Response) => {
-  const boardId = req.baseUrl.split('/')[2];
+  const boardId = req.baseUrl.split("/")[2];
   try {
     const foundedColumns = await columnService.findColumns({ boardId });
     res.json(foundedColumns);
@@ -15,20 +14,18 @@ export const getColumns = async (req: Request, res: Response) => {
 
 export const getColumnById = async (req: Request, res: Response) => {
   try {
-    const foundedColumn = await columnService.findColumnById(req.params['columnId']);
+    const foundedColumn = await columnService.findColumnById(req.params["columnId"]);
     res.json(foundedColumn);
+  } catch (err) {
+    return res.status(404).send(createError(404, "Column was not founded!"));
   }
-  catch (err) {
-    return res.status(404).send(createError(404, 'Column was not founded!'));
-  }
-
 };
 
 export const createColumn = async (req: Request, res: Response) => {
-  const guid = req.header('Guid') || 'undefined';
-  const initUser = req.header('initUser') || 'undefined';
-  const boardId = req.baseUrl.split('/')[2];
-  const bodyError = checkBody(req.body, ['title', 'order'])
+  const guid = req.header("Guid") || "undefined";
+  const initUser = req.header("initUser") || "undefined";
+  const boardId = req.baseUrl.split("/")[2];
+  const bodyError = checkBody(req.body, ["title", "order"]);
   if (bodyError) {
     return res.status(400).send(createError(400, "bad request: " + bodyError));
   }
@@ -38,33 +35,54 @@ export const createColumn = async (req: Request, res: Response) => {
   try {
     const newColumn = await columnService.createColumn({ title, order, boardId }, guid, initUser);
     res.json(newColumn);
+  } catch (err) {
+    return console.log(err);
   }
-  catch (err) { return console.log(err); }
-
 };
 
 export const updateColumn = async (req: Request, res: Response) => {
-  const guid = req.header('Guid') || 'undefined';
-  const initUser = req.header('initUser') || 'undefined';
-  const bodyError = checkBody(req.body, ['title', 'order'])
+  const guid = req.header("Guid") || "undefined";
+  const initUser = req.header("initUser") || "undefined";
+  const bodyError = checkBody(req.body, ["title", "order"]);
   if (bodyError) {
     return res.status(400).send(createError(400, "bad request: " + bodyError));
+  }
+  try {
+    await columnService.findColumnById(req.params["columnId"]);
+  } catch (err) {
+    return res.status(404).send(createError(404, "Column was not founded!"));
   }
   const { title, order } = req.body;
 
   try {
-    const updatedColumn = await columnService.updateColumn(req.params['columnId'], { title, order }, guid, initUser)
+    const updatedColumn = await columnService.updateColumn(
+      req.params["columnId"],
+      { title, order },
+      guid,
+      initUser
+    );
     res.json(updatedColumn);
+  } catch (err) {
+    return console.log(err);
   }
-  catch (err) { return console.log(err); }
 };
 
 export const deleteColumn = async (req: Request, res: Response) => {
-  const guid = req.header('Guid') || 'undefined';
-  const initUser = req.header('initUser') || 'undefined';
+  const guid = req.header("Guid") || "undefined";
+  const initUser = req.header("initUser") || "undefined";
   try {
-    const deletedColumn = await columnService.deleteColumnById(req.params['columnId'], guid, initUser);
-    res.json(deletedColumn);
+    await columnService.findColumnById(req.params["columnId"]);
+  } catch (err) {
+    return res.status(404).send(createError(404, "Column was not founded!"));
   }
-  catch (err) { return console.log(err); }
+  try {
+    const deletedColumn = await columnService.deleteColumnById(
+      req.params["columnId"],
+      guid,
+      initUser
+    );
+    res.json(deletedColumn);
+  } catch (err) {
+    return console.log(err);
+  }
 };
